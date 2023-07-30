2c4a66fc7cd8649d48880d8def10c13e0c8525fe
dolphinderby <129580551+dolphinderby@users.noreply.github.com>
2023-07-30 14:23:19 +0000

    記事作成ページ作成中

--- app/controllers/admin/articles_controller.rb
+++ app/controllers/admin/articles_controller.rb
@@ -1,13 +1,43 @@
 class Admin::ArticlesController < ApplicationController
   def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(item_params)
+    if @article.save
+      redirect_to admin_article_path(@article.id)
+    else
+      render :new
+    end
   end
 
   def index
+    @article = Article.all
   end
 
   def show
+    @article = Article.find(params[:id])
   end
 
   def edit
+    @article = Article.find(params[:id])
   end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "You have updated user successfully."
+      redirect_to admin_article_path(@article.id)
+    else
+      render :edit
+    end
+  end
+
+   private
+
+  def article_params
+    params.require(:article).permit(:name, :sub_title, :introduction, :prefecture, :venue, :event_day, :image)
+  end
+
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -11,7 +11,7 @@ class Customer < ApplicationRecord
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-    image
+    image.variant(resize_to_limit: [50, 50]).processed
   end
 
   def fullname
