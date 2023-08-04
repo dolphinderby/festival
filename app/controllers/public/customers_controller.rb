73e15099f4af1574944e357195052279cff15773
dolphinderby <129580551+dolphinderby@users.noreply.github.com>
2023-08-04 11:49:52 +0000

    いいね機能の実装でログインなしでページ開けす

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,4 +1,5 @@
 class Public::CustomersController < ApplicationController
+  before_action :set_customer, only: [:nices]
 
   def show
     @customer = current_customer
@@ -29,9 +30,19 @@ class Public::CustomersController < ApplicationController
     redirect_to root_path
   end
 
+  def nices
+    nices = Nice.where(customer_id: @customer.id).pluck(:article_id)
+    @nices = Article.find(nices)
+  end
+
   private
 
   def customer_params
     params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :nickname, :email, :image)
   end
+
+  def set_customer
+    @customer = Customer.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
 
+  devise_for :customers, controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+
+  devise_for :admin, controllers: {
+    sessions: "admin/sessions"
+  }
+
   namespace :admin do
     root :to => 'homes#top'
     resources :customers, only: [:index, :show, :edit, :update]
@@ -10,23 +19,19 @@ Rails.application.routes.draw do
 
   scope module: :public do
     root :to => 'homes#top'
+
     get 'customers/my_page' => 'customers#show'
     get 'customers/my_page/edit' => 'customers#edit'
+    get 'customers/my_page/nice' => 'customers#nice'
     patch 'customers/my_page/update' => 'customers#update'
     get 'customers/confirm' => 'customers#confirm'
     patch 'customers/withdraw' => 'customers#withdraw'
+
     resources :articles, only: [:index, :show] do
      resource :nices, only: [:create, :destroy]
     end
   end
 
-  devise_for :customers, controllers: {
-    registrations: "public/registrations",
-    sessions: 'public/sessions'
-  }
 
-  devise_for :admin, controllers: {
-    sessions: "admin/sessions"
-  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
